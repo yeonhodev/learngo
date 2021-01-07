16bb2799253700adef011baece329e60909e4e53
yeonhodev <yeonho.dev@gmail.com>
2021-01-07 00:22:52 -0500

    Modified main.go
    Added a function that creates a string array

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	totalLength, upperName := lenAndUpper("nico")
-	fmt.Println(totalLength, upperName)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
