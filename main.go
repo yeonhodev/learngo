ac6960427b38d6db607ee0df1d831376e5155975
yeonhodev <yeonho.dev@gmail.com>
2021-01-06 23:55:14 -0500

    Modified main.go

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hellow World!")
+	name := "nico"
+	// var name string = "nico" -> := 는 function 안에서만 타입을 추측해서 지정해 줌
+	name = "lynn"
+	fmt.Println(name)
 }
