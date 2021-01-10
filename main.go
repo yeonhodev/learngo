455a9d7e2942e65adb5db280a9f408e5af7ef58e
yeonhodev <yeonho.dev@gmail.com>
2021-01-09 23:48:47 -0500

    modified main.go
    sum function by using for loop

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 )
 
 func superAdd(numbers ...int) int {
-	for i := 0; i < len(numbers); i++ {
-		fmt.Println(numbers[i])
+	total := 0
+	for _, number := range numbers {
+		total += number
 	}
-	return 1
+	return total
 }
 
 func main() {
-	superAdd(1, 2, 3, 4, 5, 6)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
