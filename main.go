4ead80117264edaff36a703bc2a817aebe6e2e83
yeonhodev <yeonho.dev@gmail.com>
2021-01-09 23:46:18 -0500

    Modified main.go
    for loop 2

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func superAdd(numbers ...int) int {
-	for index, number := range numbers {
-		fmt.Println(index, number)
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
 	}
 	return 1
 }
