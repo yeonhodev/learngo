0ebb03a3f157a684e9c0d17135b61092fb932e66
yeonhodev <yeonho.dev@gmail.com>
2021-01-09 23:44:29 -0500

    Modified main.go
    for loop

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	for index, number := range numbers {
+		fmt.Println(index, number)
+	}
+	return 1
 }
 
 func main() {
-	totalLength, up := lenAndUpper("nico")
-	fmt.Println(totalLength, up)
+	superAdd(1, 2, 3, 4, 5, 6)
 }
