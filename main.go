93d360b0df16df70a72f30b5d2d6404d8f4e3868
yeonhodev <yeonho.dev@gmail.com>
2021-01-11 23:55:57 -0500

    Update main.go
    switch with variable creation

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 )
 
 func canIDrink(age int) bool {
-	switch {
-	case age < 18:
+	switch koreanAge := age +2; koreanAge {
+	case 10:
 		return false
-	case age == 18:
+	case 18:
 		return true
-	case age > 50:
-		return false
 	}
 	retur false
 }
