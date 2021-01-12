79c70727f302fca9a4fcc482342e974fb35f2ba3
yeonhodev <yeonho.dev@gmail.com>
2021-01-11 23:52:17 -0500

    Modified main.go
    switch

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 )
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch age {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	retur false
 }
 
 func main() {
