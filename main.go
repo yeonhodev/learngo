5f92dea8e1177d4b596cbd2fcdacc1f08f67ad4f
yeonhodev <yeonho.dev@gmail.com>
2021-01-14 23:02:48 -0500

    Update main.go
    print memory address

--- main.go
+++ main.go
@@ -4,16 +4,8 @@ import (
 	"fmt"
 )
 
-func canIDrink(age int) bool {
-	switch koreanAge := age +2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := 5
+	fmt.Println(&a, &b)
 }
