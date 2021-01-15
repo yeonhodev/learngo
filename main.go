8393b5e734733f8c51e9d64302ed4cb6f57b7f93
yeonhodev <yeonho.dev@gmail.com>
2021-01-14 23:09:28 -0500

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main() {
 	a := 2
 	// &a is memory address of a
 	b := &a
+	// change value of a by using pointer
+	*b = 20
 	// *b is value of the memory address
-	fmt.Println(*b)
+	fmt.Println(a)
 }
