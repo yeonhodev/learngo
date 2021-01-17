0c1d0b35fc085165681c3d89d6270432ae88f7c4
yeonhodev <yeonho.dev@gmail.com>
2021-01-16 23:46:07 -0500

    Update main.go
    Maps and for loop

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	names := []string{"nico", "lynn", "dal"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	nico := map[string]string{"name": "nico", "age": "12"}
+	for key, value := range nico {
+		fmt.Println(key, value)
+	}
 }
