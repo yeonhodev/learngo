39dd2aee70e159113a47dcdababc15a2fb5aebff
yeonhodev <yeonho.dev@gmail.com>
2021-01-17 23:33:29 -0500

    Update main.go
    struct

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+type person struct {
+	name string
+	age int
+	favFood []string
+}
 
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{"nico", 18, favFood}
+	fmt.Println(nico)
 }
