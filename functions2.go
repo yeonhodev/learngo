c22fd105863a9418dc894a651ae9899dc3e37307
yeonho.dev <yeonho.dev@gmail.com>
2021-01-26 23:47:59 -0500

    added functions2.go

---
+++ functions2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/* 함수2 - 일급 함수, 익명 함수, 클로저(Closure)
+Go 언어의 함수는 일련의 코드를 실행해 주는 함수의 역할 뿐만 아니라,
+변수에 할당할 수 있고, 함수의 인자나 리턴 값이 될 수도 있다.
+*/
+
+func hamsu() {
+	fmt.Println("나는 함수다!")
+}
+
+func main() {
+
+}
