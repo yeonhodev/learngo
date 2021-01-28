d5fdad95d6539be4a60610af7013f7b255238a43
yeonho.dev <yeonho.dev@gmail.com>
2021-01-27 23:59:01 -0500

    closure1

--- functions2.go
+++ functions2.go
@@ -7,13 +7,13 @@ Go 언어의 함수는 일련의 코드를 실행해 주는 함수의 역할 뿐
 변수에 할당할 수 있고, 함수의 인자나 리턴 값이 될 수도 있다.
 */
 
-func hamsu2(myHamsu func(int) int) {
-	fmt.Println(myHamsu(7))
+func returnHamsu(x string) func() {
+	return func() { fmt.Println(x) }
 }
 
 func main() {
-	hamsu := func(x int) int {
-		return x * -1
-	}
-	hamsu2(hamsu)
+	x := returnHamsu("나는 함수!")
+	y := returnHamsu("너도 함수!")
+	x()
+	y()
 }
