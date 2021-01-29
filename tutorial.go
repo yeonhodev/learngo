69c31dd8e9c634d354f2aea29cfb80d71a3d527b
yeonho.dev <yeonho.dev@gmail.com>
2021-01-28 23:10:02 -0500

    function closure

--- tutorial.go
+++ tutorial.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
-func test2(myFunc func(int) int) {
-	fmt.Println(myFunc(7))
+func returnFunc(x string) func() {
+	return func() {
+		fmt.Println(x)
+	}
 }
 
 func main() {
-	test := func(x int) int {
-		return x * -1
-	}
-	test2(test)
+	returnFunc("hello")()
+	x := returnFunc("goodbye")
+	x()
 }
