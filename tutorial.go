1596d203828edbb699cd09b2a50b62a6c0960c4b
yeonho.dev <yeonho.dev@gmail.com>
2021-01-29 23:49:57 -0500

    variable

--- tutorial.go
+++ tutorial.go
@@ -2,14 +2,10 @@ package main
 
 import "fmt"
 
-func returnFunc(x string) func() {
-	return func() {
-		fmt.Println(x)
-	}
-}
-
 func main() {
-	returnFunc("hello")()
-	x := returnFunc("goodbye")
-	x()
+	var name string
+	name = "Tim"
+	name = "Bill"
+
+	fmt.Println("Hello World!")
 }
