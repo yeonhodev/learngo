256d1d5147163330e6f40cd3cf528e782cf67136
yeonho.dev <yeonho.dev@gmail.com>
2021-02-03 00:16:34 -0500

    get user input and convert string

--- tutorial.go
+++ tutorial.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type the year you were born: ")
 	scanner.Scan()
-	input := scanner.Text()
-	fmt.Printf()
+	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	fmt.Printf("You will be %d years old at the end of 2021", 2021-input)
 }
