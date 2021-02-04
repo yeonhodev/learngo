113d8a09b9f8ff4a5af62487a0852667a4f499e1
yeonho.dev <yeonho.dev@gmail.com>
2021-02-03 23:02:25 -0500

    Arithmetic Operators

--- tutorial.go
+++ tutorial.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Type the year you were born: ")
-	scanner.Scan()
-	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
-	fmt.Printf("You will be %d years old at the end of 2021", 2021-input)
+	var num1 float32 = 9
+	var num2 float32 = 4
+	answer := num1 / num2
+	fmt.Printf("%g", answer)
 }
