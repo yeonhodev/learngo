c0349b68102167bfe97cd8c0ed6400faf92eb2b3
yeonho.dev <yeonho.dev@gmail.com>
2021-02-08 23:50:40 -0500

    arrays

--- tutorial.go
+++ tutorial.go
@@ -5,14 +5,8 @@ import (
 )
 
 func main() {
-	ans := 10
-
-	switch {
-	case ans > 0:
-		fmt.Println("greater than 0")
-	case ans < 0:
-		fmt.Println("less than 0")
-	default:
-		fmt.Println("zero")
-	}
+	var arr [5]int
+	arr[0] = 100
+	arr[4] = 900
+	fmt.Println(arr)
 }
