cad4f8a33ee275ed255bf8bfa0d78fc08f39488f
yeonho.dev <yeonho.dev@gmail.com>
2021-02-08 23:55:09 -0500

    array and for loop

--- tutorial.go
+++ tutorial.go
@@ -1,12 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var arr [5]int
-	arr[0] = 100
-	arr[4] = 900
-	fmt.Println(arr)
+	arr := [3]int{4, 5, 6}
+	sum := 0
+
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	}
+	fmt.Println(sum)
 }
