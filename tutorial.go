fd841c91066fb356b92ff8bbe88a9aa29dc76119
yeonho.dev <yeonho.dev@gmail.com>
2021-02-09 00:00:35 -0500

    2D array

--- tutorial.go
+++ tutorial.go
@@ -6,6 +6,10 @@ func main() {
 	arr := [3]int{4, 5, 6}
 	sum := 0
 
+	arr2D := [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println(arr2D)
+	fmt.Println(arr2D[0][2])
+
 	for i := 0; i < len(arr); i++ {
 		sum += arr[i]
 	}
