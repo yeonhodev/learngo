1dded65f1a3bae00342d93102d16d1faa82da4c7
yeonho.dev <yeonho.dev@gmail.com>
2021-02-09 23:41:58 -0500

    slices

--- tutorial.go
+++ tutorial.go
@@ -3,15 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	arr := [3]int{4, 5, 6}
-	sum := 0
-
-	arr2D := [2][3]int{{1, 2, 3}, {4, 5, 6}}
-	fmt.Println(arr2D)
-	fmt.Println(arr2D[0][2])
-
-	for i := 0; i < len(arr); i++ {
-		sum += arr[i]
-	}
-	fmt.Println(sum)
+	var x [5]int = [5]int{1, 2, 3, 4, 5}
+	var s []int = x[1:3]
+	fmt.Println(s)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+	fmt.Println(s[:cap(s)])
 }
