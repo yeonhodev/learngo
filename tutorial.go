6d83c343273a8c5b277758500d081d8153af7877
yeonho.dev <yeonho.dev@gmail.com>
2021-02-10 00:04:44 -0500

    slices 2

--- tutorial.go
+++ tutorial.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var x [5]int = [5]int{1, 2, 3, 4, 5}
-	var s []int = x[1:3]
-	fmt.Println(s)
-	fmt.Println(len(s))
-	fmt.Println(cap(s))
-	fmt.Println(s[:cap(s)])
+	var a []int = []int{5, 6, 7, 8, 9}
+	fmt.Println(cap(a))
+	fmt.Println(cap(a[:3]))
+	a = append(a, 10)
+	fmt.Println(a)
+
+	b := make([]int, 5)
+	fmt.Println(b)
+	fmt.Printf("%T", b)
 }
