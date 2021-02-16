da28efd2483bd2024eec57df6323b82c826248b6
yeonho.dev <yeonho.dev@gmail.com>
2021-02-15 23:49:43 -0500

    advanced function 2

--- tutorial.go
+++ tutorial.go
@@ -2,9 +2,13 @@ package main
 
 import "fmt"
 
+func test2(myFunc func(int) int) {
+	fmt.Println(myFunc(7))
+}
+
 func main() {
 	test := func(x int) int {
 		return x * -1
-	}(8)
-	fmt.Println(test)
+	}
+	test2(test)
 }
