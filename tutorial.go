ee8654be83d9e0e913a4ebabb5f933abcb73ebf2
yeonho.dev <yeonho.dev@gmail.com>
2021-02-15 23:40:40 -0500

    advanced function

--- tutorial.go
+++ tutorial.go
@@ -2,15 +2,9 @@ package main
 
 import "fmt"
 
-func add(x, y int) (z1, z2 int) {
-	defer fmt.Println("hello")
-	z1 = x + y
-	z2 = x - y
-	fmt.Println("before return")
-	return
-}
-
 func main() {
-	ans1, ans2 := add(5, 7)
-	fmt.Println(ans1, ans2)
+	test := func(x int) int {
+		return x * -1
+	}(8)
+	fmt.Println(test)
 }
