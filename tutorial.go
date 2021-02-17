e2f9ee977a308667dbdc8fe30e41d614d17f5ddc
yeonho.dev <yeonho.dev@gmail.com>
2021-02-16 23:35:36 -0500

    mutable and immutable data types

--- tutorial.go
+++ tutorial.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
-func returnFunc(x string) func() {
-	return func() { fmt.Println(x) }
-}
-
+//mutable data type
 func main() {
-	returnFunc("hello")()
-	x := returnFunc("goodbye")
-	x()
+	var x []int = []int{3, 4, 5}
+	y := x
+	y[0] = 100
+	fmt.Println(x, y)
+
+	var a map[string]int = map[string]int{"hello": 3}
+	b := a
+	b["b"] = 100
+	fmt.Println(a, b)
+
+	var c [2]int = [2]int{3, 4}
+	d := c
+	d[0] = 100
+	fmt.Println(c, d)
 }
