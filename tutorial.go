c824de329c0dd8741f8f26bb43be44a8af41590e
yeonho.dev <yeonho.dev@gmail.com>
2021-02-18 22:51:41 -0500

    pointers and dereference operator

--- tutorial.go
+++ tutorial.go
@@ -2,27 +2,10 @@ package main
 
 import "fmt"
 
-func changeFirst(slice []int) {
-	slice[0] = 1000
-}
-
-//mutable data type
 func main() {
-	var x []int = []int{3, 4, 5}
-	y := x
-	y[0] = 100
+	x := 7
+	y := &x
 	fmt.Println(x, y)
-
-	var a map[string]int = map[string]int{"hello": 3}
-	b := a
-	b["b"] = 100
-	fmt.Println(a, b)
-
-	var c [2]int = [2]int{3, 4}
-	d := c
-	d[0] = 100
-	fmt.Println(c, d)
-
-	changeFirst(x)
-	fmt.Println(x)
+	*y = 8
+	fmt.Println(x, y)
 }
