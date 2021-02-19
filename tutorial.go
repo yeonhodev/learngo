4e3f9903e7b415ef3bd98062b0cf9b544095b0f8
yeonho.dev <yeonho.dev@gmail.com>
2021-02-18 23:08:56 -0500

    pointers and dereference operators 3

--- tutorial.go
+++ tutorial.go
@@ -2,18 +2,8 @@ package main
 
 import "fmt"
 
-func changeValue(str *string) {
-	*str = "changed!"
-}
-
-func changeValue2(str string) string {
-	str = "changed!"
-	return str
-}
-
 func main() {
 	toChange := "hello"
-	fmt.Println(toChange)
-	changeValue(&toChange)
-	fmt.Println(toChange)
+	var pointer *string = &toChange
+	fmt.Println(*pointer)
 }
