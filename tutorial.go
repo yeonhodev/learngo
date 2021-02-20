8556e0e3b0e8186e5e22ab23eac0c29b9d87aa98
yeonho.dev <yeonho.dev@gmail.com>
2021-02-19 23:45:11 -0500

    Structs and custom types 3

--- tutorial.go
+++ tutorial.go
@@ -7,13 +7,12 @@ type Point struct {
 	y int32
 }
 
-func changeX(pt *Point) {
-	pt.x = 100
+type Circle struct {
+	radius float64
+	center *Point
 }
 
 func main() {
-	p1 := &Point{y: 3}
-	fmt.Println(p1)
-	changeX(p1)
-	fmt.Println(p1)
+	c1 := Circle{4.56, &Point{4, 5}}
+	fmt.Println(c1.center, c1.center.x)
 }
