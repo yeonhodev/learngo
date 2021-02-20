095856569b2336c976a4b1a4c5c7e4578f3e55c3
yeonho.dev <yeonho.dev@gmail.com>
2021-02-19 23:27:39 -0500

    Structs and custom types

--- tutorial.go
+++ tutorial.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+type Point struct {
+	x int32
+	y int32
+}
+
 func main() {
-	toChange := "hello"
-	var pointer *string = &toChange
-	fmt.Println(*pointer)
+	var p1 Point = Point{1, 2}
+	//var p2 Point = Point{-5, 7}
+	fmt.Println(p1.x)
 }
