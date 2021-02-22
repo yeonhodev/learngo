958e5c4c282c256e064f25304d5a391158cc35f0
yeonho.dev <yeonho.dev@gmail.com>
2021-02-22 00:11:06 -0500

    struct method max

--- tutorial.go
+++ tutorial.go
@@ -12,7 +12,7 @@ type Student struct {
 	s.age = age
 }*/
 
-func (s Student) getAverageGrade() float32 {
+func (s *Student) getAverageGrade() float32 {
 	sum := 0
 	for _, v := range s.grades {
 		sum += v
@@ -20,8 +20,21 @@ func (s Student) getAverageGrade() float32 {
 	return float32(sum) / float32(len(s.grades))
 }
 
+func (s *Student) getMaxGrade() int {
+	curMax := 0
+
+	for _, v := range s.grades {
+		if curMax < v {
+			curMax = v
+		}
+	}
+	return curMax
+}
+
 func main() {
 	s1 := Student{"Tim", []int{70, 90, 80, 85}, 19}
-	average := s1.getAverageGrade()
-	fmt.Println(average)
+	s2 := Student{"Jeo", []int{70, 90, 80, 85, 90, 95}, 21}
+	max := s1.getMaxGrade()
+	max2 := s2.getMaxGrade()
+	fmt.Println(max, max2)
 }
