c387a718170ecc98ce73853b4eabb1869f6a3788
yeonho.dev <yeonho.dev@gmail.com>
2021-02-22 00:05:09 -0500

    struct method for loop

--- tutorial.go
+++ tutorial.go
@@ -8,13 +8,20 @@ type Student struct {
 	age    int
 }
 
-func (s *Student) setAge(age int) {
+/*func (s *Student) setAge(age int) {
 	s.age = age
+}*/
+
+func (s Student) getAverageGrade() float32 {
+	sum := 0
+	for _, v := range s.grades {
+		sum += v
+	}
+	return float32(sum) / float32(len(s.grades))
 }
 
 func main() {
 	s1 := Student{"Tim", []int{70, 90, 80, 85}, 19}
-	fmt.Println(s1)
-	s1.setAge(7)
-	fmt.Print(s1)
+	average := s1.getAverageGrade()
+	fmt.Println(average)
 }
