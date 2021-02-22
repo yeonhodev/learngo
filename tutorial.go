22a7f7cac3539b87280d0e33aea9c84aa74f871a
yeonho.dev <yeonho.dev@gmail.com>
2021-02-21 23:58:13 -0500

    struct method pointer

--- tutorial.go
+++ tutorial.go
@@ -8,11 +8,13 @@ type Student struct {
 	age    int
 }
 
-func (s Student) getAge() int {
-	return s.age
+func (s *Student) setAge(age int) {
+	s.age = age
 }
 
 func main() {
 	s1 := Student{"Tim", []int{70, 90, 80, 85}, 19}
-	fmt.Println(s1.getAge())
+	fmt.Println(s1)
+	s1.setAge(7)
+	fmt.Print(s1)
 }
